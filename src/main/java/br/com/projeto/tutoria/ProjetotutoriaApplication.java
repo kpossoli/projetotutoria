5e2dd509746c89db5b4c5c81de61f2fb115ec53a
kpossoli <153238497+kpossoli@users.noreply.github.com>
2024-04-02 20:44:17 -0300

    Initial commit

---
+++ src/main/java/br/com/projeto/tutoria/ProjetotutoriaApplication.java
@@ -0,0 +1,13 @@
+package br.com.projeto.tutoria;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetotutoriaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetotutoriaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/projeto/tutoria/ProjetotutoriaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.projeto.tutoria;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetotutoriaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
