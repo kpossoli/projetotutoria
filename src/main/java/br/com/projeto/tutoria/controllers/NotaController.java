4d8295f3ba0c9fbcb4ec096bf28d279080865bf5
lucascarnu <lucascarnu@gmail.com>
2024-04-04 23:28:33 -0300

    card6-testar

--- src/main/java/br/com/projeto/tutoria/controllers/NotaController.java
+++ src/main/java/br/com/projeto/tutoria/controllers/NotaController.java
@@ -1,6 +1,7 @@
 package br.com.projeto.tutoria.controllers;
 
 
+import br.com.projeto.tutoria.dto.NotaLancamentoDTO;
 import br.com.projeto.tutoria.entities.NotaEntity;
 import br.com.projeto.tutoria.services.NotaService;
 import lombok.RequiredArgsConstructor;
@@ -32,6 +33,12 @@ public class NotaController {
         return ResponseEntity.status(HttpStatus.CREATED).body(service.criar(nota));
     }
 
+    @PostMapping("/lancar")
+    public ResponseEntity<NotaEntity> lancarNota(@RequestBody NotaLancamentoDTO notaLancamentoDTO) {
+        NotaEntity novaNota = service.lancarNota(notaLancamentoDTO);
+        return ResponseEntity.status(HttpStatus.CREATED).body(novaNota);
+    }
+
     @PutMapping("{id}")
     public ResponseEntity<NotaEntity> alterarNota(@PathVariable Long id, @RequestBody NotaEntity nota) {
         return ResponseEntity.status(HttpStatus.OK).body(service.alterar(id, nota));
@@ -41,6 +48,12 @@ public class NotaController {
     public ResponseEntity<Void> deletarNota(@PathVariable Long id) {
         service.excluir(id);
         return ResponseEntity.noContent().build();
+    }
+
+    @GetMapping("/por-matricula/{matriculaId}")
+    public ResponseEntity<List<NotaEntity>> buscarNotasPorMatriculaId(@PathVariable Long matriculaId) {
+        List<NotaEntity> notas = service.buscarNotasPorMatriculaId(matriculaId);
+        return notas.isEmpty() ? ResponseEntity.noContent().build() : ResponseEntity.ok(notas);
     }
 
 
--- src/main/java/br/com/projeto/tutoria/services/NotaServiceImpl.java
+++ src/main/java/br/com/projeto/tutoria/services/NotaServiceImpl.java
@@ -1,19 +1,29 @@
 package br.com.projeto.tutoria.services;
 
+import br.com.projeto.tutoria.dto.NotaLancamentoDTO;
+import br.com.projeto.tutoria.entities.DisciplinaMatriculaEntity;
 import br.com.projeto.tutoria.entities.NotaEntity;
 import br.com.projeto.tutoria.exceptions.NotFoundException;
+import br.com.projeto.tutoria.repositories.DisciplinaMatriculaRepository;
 import br.com.projeto.tutoria.repositories.NotaRepository;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
+import java.math.BigDecimal;
+import java.math.RoundingMode;
 import java.util.List;
 
 @Service
 public class NotaServiceImpl implements NotaService {
 
     private final NotaRepository repository;
+    private DisciplinaMatriculaRepository disciplinaMatriculaRepository;
 
-    public NotaServiceImpl(NotaRepository repository) {
+    public NotaServiceImpl(NotaRepository repository, DisciplinaMatriculaRepository disciplinaMatriculaRepository) {
         this.repository = repository;
+        this.disciplinaMatriculaRepository = disciplinaMatriculaRepository;
     }
 
     @Override
@@ -41,7 +51,62 @@ public class NotaServiceImpl implements NotaService {
 
     @Override
     public void excluir(Long id) {
-        NotaEntity entity = buscarPorId(id);
-        repository.delete(entity);
+        NotaEntity nota = buscarPorId(id); // Busca a nota para obter o ID da matrícula
+        Long matriculaId = nota.getDisciplinaMatricula().getId(); // Obtem o ID da matrícula
+        repository.delete(nota); // Exclui a nota
+        atualizarMediaFinal(matriculaId); // Recalcula a média final da matrícula
+    }
+
+
+    public NotaEntity lancarNota(NotaLancamentoDTO notaLancamentoDTO) {
+        DisciplinaMatriculaEntity matricula = disciplinaMatriculaRepository.findById(notaLancamentoDTO.getDisciplinaMatriculaId())
+                .orElseThrow(() -> new NotFoundException("Matrícula não encontrada com id: " + notaLancamentoDTO.getDisciplinaMatriculaId()));
+
+        NotaEntity nota = new NotaEntity();
+        nota.setDisciplinaMatricula(matricula);
+        nota.setNota(notaLancamentoDTO.getNota());
+        nota.setCoeficiente(notaLancamentoDTO.getCoeficiente());
+        nota.setProfessor(matricula.getDisciplina().getProfessor());
+        NotaEntity notaSalva = repository.save(nota);
+
+        atualizarMediaFinal(matricula.getId());
+
+        return notaSalva;
+    }
+
+    @Override
+    public List<NotaEntity> buscarNotasPorMatriculaId(Long matriculaId) {
+        return repository.findByDisciplinaMatriculaId(matriculaId);
+    }
+
+
+    private void atualizarMediaFinal(Long matriculaId) {
+        List<NotaEntity> notas = repository.findByDisciplinaMatriculaId(matriculaId);
+        if (notas.isEmpty()) {
+            setMediaFinal(matriculaId, BigDecimal.ZERO);
+        } else {
+            BigDecimal somaPesos = BigDecimal.ZERO;
+            BigDecimal somaNotasPonderadas = BigDecimal.ZERO;
+
+            // Iterar sobre cada nota para calcular a soma dos pesos e a soma ponderada das notas
+            for (NotaEntity nota : notas) {
+                BigDecimal coeficiente = nota.getCoeficiente();
+                BigDecimal notaPonderada = nota.getNota().multiply(coeficiente);
+
+                somaPesos = somaPesos.add(coeficiente);
+                somaNotasPonderadas = somaNotasPonderadas.add(notaPonderada);
+            }
+
+            // Calcular a média final
+            BigDecimal mediaFinal = somaNotasPonderadas.divide(somaPesos, 2, RoundingMode.HALF_UP);
+            setMediaFinal(matriculaId, mediaFinal);
+        }
+    }
+
+    private void setMediaFinal(Long matriculaId, BigDecimal mediaFinal) {
+        DisciplinaMatriculaEntity matricula = disciplinaMatriculaRepository.findById(matriculaId)
+                .orElseThrow(() -> new NotFoundException("Matrícula não encontrada com id: " + matriculaId));
+        matricula.setMediaFinal(mediaFinal);
+        disciplinaMatriculaRepository.save(matricula);
     }
 }
